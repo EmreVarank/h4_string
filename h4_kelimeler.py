de41ab086eb35aec05fab3713442359e8e2837f8
Emre <yunusemrevarank@hotmail.com>
2025-03-10 22:21:13 +0300

    Delete h4_kelimeler.py

--- h4_kelimeler.py
+++
@@ -1,3 +0,0 @@
-sayac=[]
-g='Data science is revolutionizing industries from business to healthcare and education. However, success in fields such as data analysis and machine learning algorithms goes beyond just collecting and processing data. These processes must be supported by solid mathematical foundations and analytical techniques. In this article, we will explore six key mathematical theories that are transforming data science: calculus of variations, game theory, vector algebra and vector calculus, probability theory, operations research, and wave theory. We will examine the place and contribution of each in data science applications.'
-kelimeler=g.split(" ") #arada boşluk bırakılan her şeyi tek tek kelimeler dizisine at
